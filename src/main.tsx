19a6e346190e431a72f27a9fae9d30f978081e63
Nico Zamora <dev@zico.xyz>
2024-06-14 18:56:48 -0400

    Init antd

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,12 @@ import { RouterProvider } from '@tanstack/react-router'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '@/lib/query'
 import { router } from '@/lib/router'
+import { ConfigProvider } from 'antd'
+
+const themeConfig = {
+  colorPrimary: '#00b96b',
+  borderRadius: 2,
+}
 
 const rootElement = document.getElementById('app') as HTMLDivElement
 if (!rootElement.innerHTML) {
@@ -14,7 +20,9 @@ if (!rootElement.innerHTML) {
   root.render(
     <StrictMode>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
+        <ConfigProvider theme={{ token: themeConfig }}>
+          <RouterProvider router={router} />
+        </ConfigProvider>
       </QueryClientProvider>
     </StrictMode>,
   )
